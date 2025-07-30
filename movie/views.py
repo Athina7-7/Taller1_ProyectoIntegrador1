17d15d3b5717d3d46ffc29be8886fbc2c1c16e54
Athina Cappelletti <84542209+Athina7-7@users.noreply.github.com>
2025-07-30 10:19:26 -0500

    Primer commit

---
+++ movie/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    # return render(request, 'home.html')
+    #return HttpResponse('<h1>Welcome to home page</h1>')
+    return render(request, 'home.html', {'name': 'Athina Cappelletti'})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
+
