c937f7c79757d02ca5ccd138dd274c4e1155be4d
Athina Cappelletti <84542209+Athina7-7@users.noreply.github.com>
2025-07-30 11:36:02 -0500

    Busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Movie
 # Create your views here.
 
 
 def home(request):
     # return render(request, 'home.html')
     #return HttpResponse('<h1>Welcome to home page</h1>')
-    return render(request, 'home.html', {'name': 'Athina Cappelletti'})
+    # return render(request, 'home.html', {'name': 'Athina Cappelletti'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchMovie': searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
